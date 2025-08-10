c405c7b0046323139cc68917102527616168f85a
AlandersonSantos <alandersonlima089@gmail.com>
2025-08-09 23:03:48 -0300

    explicando

---
+++ tiposDeDados/tipos.py
@@ -0,0 +1,30 @@
+#Tipo inteiro(INT):
+
+print(11 + 10) #retorna 21
+
+print(33 + 7) #retorna 40
+
+print(1000 + 100 + 10) #retorna 1110
+
+
+#tipo flutuante(FLOAT):
+
+print(1.5 + 5 + 1) #retorna 7.5
+
+print(0.5 + 0.3) #retorna 0.8
+
+print(0.2 + 0.9) #retorna 1.1
+
+#tipos texto/string(STR):
+
+print("Python") #retorna Python
+
+print("Olá mundo") #retorna Olá mundo
+
+print("Foco") #retorna Foco
+
+#tipo booleano(BOOL):
+
+print(True) #retorna True
+
+print(False) #retorna False
